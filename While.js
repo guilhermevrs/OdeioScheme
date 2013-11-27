14a27d0235b45231ada5f2bc0bd39a02d9a8e959
Guilherme Schwade <guilhermevrs@gmail.com>
2013-11-27 00:07:49 -0200

    Finalizado v.1.0

--- While.js
+++
@@ -1,9 +0,0 @@
-/*
-	While
-	*/
-	var WhileCommand = function(condition, trueExec){
-	this.condition = (typeof condition === 'undefined')? null : condition;
-	this.trueExec = (typeof trueExec === 'undefined')? null : trueExec;
-}
-
-module.exports = WhileCommand;
