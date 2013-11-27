13aee9e366eeb540dcb2b141b5ebaac4304e80b6
Guilherme Schwade <guilhermevrs@gmail.com>
2013-11-26 22:16:15 -0200

    IfCommand refatorado

--- IfCommand.js
+++ IfCommand.js
@@ -5,6 +5,17 @@ var IfCommand = function(condition, trueExec, falseExec){
 	this.condition = (typeof condition === 'undefined')? null : condition;
 	this.trueExec = (typeof trueExec === 'undefined')? null : trueExec;
 	this.falseExec = (typeof falseExec === 'undefined')? null : falseExec;
+	this.step = function(){
+		c = new Common();
+		if(!c.isValue(this.condition)){
+			this.condition = this.condition.step();
+			return this;
+		}
+		if(this.condition)
+			return this.trueExec;
+		else
+			return this.falseExec;
+	}
 }
 
 module.exports = IfCommand;
